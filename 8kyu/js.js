8d5d2bdcdb6e0040baaa42cbe6f5b8bd91a5e32f
gakuru2006 <163123799+gakuru2006@users.noreply.github.com>
2025-05-24 17:27:20 +0200

    welcome to the city challeng

---
+++ 8kyu/js.js
@@ -0,0 +1,10 @@
+function sayHello( name, city, state ) {
+    //jion names with spaces
+    let name=['Gakuru','Elijah'];
+    let city="kigali";
+    let state="rwanda";
+     fullName = name.join(" ");
+      
+return 'Hello , ${fullName}!  Welcome to {city} , {state}!';
+      
+    }
